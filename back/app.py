c5cd9ce2660546081ccb8def28593bd53caa5f7c
NelsonM9 <nextnelson1234@gmail.com>
2020-10-02 23:05:45 -0500

    en el app se inicializa las rutas, la db y se configura la db

--- back/app.py
+++ back/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from routes import *
 from flask_cors import CORS
-from routes import consultant, login, customer, equipment, line, bill
+from db.model import *
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['SQLALCHEMY_DATABASE_URI']= "postgresql://postgres:pass1234@localhost:5432/Estetica_belleza_sh"
+
+CORS(app, supports_credentials= True)
+db.init_app(app)
+
+app.add_url_rule(client["signin"], view_func= client["view_func_signin"])
+
+if __name__ == "__main__":
+    app.run(port= 5000, debug= True)
