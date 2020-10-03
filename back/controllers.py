1f4543bbd6af1ab9332b41d9dab0ee512d9b7319
NelsonM9 <nextnelson1234@gmail.com>
2020-10-02 23:07:50 -0500

    se crea la clase para encriptar el password y la clase para el datamanager

--- back/controllers.py
+++ back/controllers.py
@@ -1,3 +1,34 @@
-from config import KEY_TOKEN_AUTH
+from flask import request, jsonify
+from db.model import Client
+from marshmallow import validate
 from flask.views import MethodView
-from flask import Flask, render_template, request, session, redirect, url_for, jsonify
+from helpers.data_manager import DataManager
+from helpers.encrypt_pass import EncryptPass
+from validators.client import ClientRegistration
+
+data_m = DataManager()
+encrypt = EncryptPass()
+client_schema = ClientRegistration()
+
+class Signin(MethodView):
+
+    def post(self):
+        try:
+            client_signin = request.get_json()
+            errors = client_schema.validate(client_signin)
+            if errors:
+                print(errors)
+                return jsonify({'state':'error','error':errors})
+            new_client = Client(
+                name_c = client_signin['name'],
+                lastname_c = client_signin['last_Name'],
+                email = client_signin['email'],
+                password = client_signin['password'],
+                favorite_one = client_signin['favorite_one'],
+                favorite_two = client_signin['favorite_two'],
+                favorite_three = client_signin['favorite_three']
+            )
+            state = data_m.add(new_client)
+            return jsonify({'state':state})
+        except:
+            return jsonify({'state':'error'})
